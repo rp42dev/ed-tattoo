22a305e4e3076f67e71fce7b24e5d2a381355dd9
Raivis Petrovskis <rapet80@gmail.com>
2022-09-28 21:11:12 +0100

    add home template

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -2,9 +2,10 @@ import { h } from 'preact';
 import { Link } from 'preact-router/match';
 import style from './style';
 
+
 const Header = () => (
 	<header class={style.header}>
-		<Link href="/"><h1>Jane Doe</h1></Link>
+		<Link href="/"><h1>Ed Tattoo</h1></Link>
 		<nav>
 			<Link activeClassName={style.active} href="/blogs">Blogs</Link>
 			<Link activeClassName={style.active} href="/contact">Contact me</Link>
